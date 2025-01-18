8cfda0c9e115683fdc3b196ff2c45a422bed080f
larabatista8 <larasantosb8@gmail.com>
2025-01-18 13:56:54 -0300

    adicionando funções na main

--- U4_Cap2_Atividade1.c
+++ U4_Cap2_Atividade1.c
@@ -18,3 +18,32 @@ void liga_led_green(){
     sleep_ms(3000);
     gpio_put(led_pin_green, 0);
 }
+
+
+//main
+int main(){
+  //inicializando LEDs e teclado
+  inicializa_LEDs();
+  inicializa_teclado();
+  while(true){
+    //lendo a tecla
+    char tecla = ler_tecla();
+    //se for 8,chama a função que liga todos os LEDs
+    if(tecla == '8' ){
+      liga_todos_LEDs();}
+    sleep_ms(100);
+    // liga led verde
+    if (tecla == '1'){
+        liga_led_green();
+        }
+    // liga led azul
+    else if (tecla == '2'){
+        liga_led_blue();
+    }
+    // liga led vermelho
+    else if(tecla == '3') {
+        liga_led_red();
+    }
+  }
+return 0;
+}
